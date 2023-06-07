effeae4b41778065c86fa52778eea4ef23cf1c5d
Franklin Rodriguez <franklinserif@gmail.com>
2023-06-06 22:03:18 -0400

    refactor: :recycle: Add src props to Avatar component

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -3,14 +3,18 @@
 import { FC } from "react";
 import Image from "next/image";
 
-const Avatar: FC = () => {
+interface Props {
+  src: string | null | undefined;
+}
+
+const Avatar: FC<Props> = ({ src }) => {
   return (
     <Image
       className="rounded-full "
       height="30"
       width="30"
       alt="Avatar"
-      src="/images/placeholder.jpg"
+      src={src || "/images/placeholder.jpg"}
     />
   );
 };
