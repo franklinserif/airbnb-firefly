4ea4c5cec9e4e40814127646a256761dbd506cac
Franklin Rodriguez <franklinserif@gmail.com>
2023-06-08 07:28:00 -0400

    feat: :sparkles: Add RentModal

---
+++ app/components/modals/RentModal.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import { FC, useMemo, useState } from "react";
+import Modal from "./Modal";
+import useRentModal from "@/app/hooks/useRentModal";
+import Heading from "../Heading";
+import { categories } from "../navbar/Categories";
+import CategoryInput from "../inputs/CategoryInput";
+import { FieldValues, useForm } from "react-hook-form";
+
+enum STEPS {
+  CATEGORY = 0,
+  LOCATION = 1,
+  INFO = 2,
+  IMAGES = 3,
+  DESCRIPTION = 4,
+  PRICE = 5,
+}
+
+const RentModal: FC = () => {
+  const rentModal = useRentModal();
+
+  const [step, setStep] = useState(STEPS.CATEGORY);
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      category: "",
+      location: "",
+      guestCount: 1,
+      roomCount: 1,
+      bathroomCount: 1,
+      imageSrc: "",
+      price: 1,
+      title: "",
+      description: "",
+    },
+  });
+
+  const category = watch("category");
+
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldDirty: true,
+      shouldTouch: true,
+      shouldValidate: true,
+    });
+  };
+
+  const handleBack = () => {
+    setStep((step) => step - 1);
+  };
+
+  const onNext = () => {
+    setStep((step) => step + 1);
+  };
+
+  const actionLabel = useMemo(() => {
+    if (step === STEPS.PRICE) {
+      return "Create";
+    }
+
+    return "Next";
+  }, [step]);
+
+  const secondaryActionLAbel = useMemo(() => {
+    if (step === STEPS.CATEGORY) {
+      return undefined;
+    }
+
+    return "Back";
+  }, [step]);
+
+  let bodyContent = (
+    <div className="flex flex-col gap-8">
+      <Heading
+        title="Which is these best describes your place"
+        subtitle="Pick a category"
+      />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
+        {categories.map((item) => (
+          <div key={item.label} className="col-span-1">
+            <CategoryInput
+              icon={item.icon}
+              label={item.label}
+              selected={category === item.label}
+              onClick={(category) => setCustomValue("category", category)}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+
+  return (
+    <Modal
+      title="Airbnb firefly your home"
+      isOpen={rentModal.isOpen}
+      onSubmit={rentModal.onClose}
+      onClose={rentModal.onClose}
+      actionLabel={actionLabel}
+      secondaryActionLabel={secondaryActionLAbel}
+      secondaryAction={step === STEPS.CATEGORY ? undefined : handleBack}
+      body={bodyContent}
+    />
+  );
+};
+
+export default RentModal;
