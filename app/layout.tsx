f658ccae2f775cc3823bea612e31429890573932
Franklin Rodriguez <franklinserif@gmail.com>
2023-06-08 07:26:27 -0400

    feat: :sparkles: Add RentModal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Nunito } from "next/font/google";
 import Navbar from "./components/navbar/Navbar";
 import RegisterModal from "./components/modals/RegisterModal";
 import LoginModal from "./components/modals/LoginModal";
+import RentModal from "./components/modals/RentModal";
 import ToasterProvider from "./providers/ToasterProvider";
 import getCurrentUser from "./actions/getCurrentUser";
 
@@ -26,6 +27,7 @@ export default async function RootLayout({
         <ToasterProvider />
         <RegisterModal />
         <LoginModal />
+        <RentModal />
         <Navbar currentUser={currentUser} />
         {children}
       </body>
